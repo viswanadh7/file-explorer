83e16f864b34a34af86cc00d19ad0e54155f97d7
viswanadh7 <mudunuruviswanadh@gmail.com>
2025-12-02 19:17:10 +0530

    style(statusbar): set status bar style to dark content

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
+  StatusBar.setBarStyle("dark-content");
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
